e6704c7aa4a6793f2c257215165674a03f26fc8e
Sebastian Cichosz <foobarbaz@gazeta.pl>
2017-01-18 00:10:37 +0100

    Add support for non-IR regions.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Oracle APEX Dynamic Action Plugin - Floating Scrollbar
-This plugin enables to stick horizontal scrollbar of an item or region to the bottom of a page.
+This plugin enables to stick horizontal scrollbar of a region to the bottom of a page.
 
 Plugin is based on Ben Alman's [jQuery Floating Scrollbar](https://gist.github.com/cowboy/846423).
 
--- server/js/add-floating-scrollbar.js
+++ server/js/add-floating-scrollbar.js
@@ -1,3 +1,16 @@
 function addFloatingScrollbar() {
-  this.affectedElements.find('table.a-IRR-table:last').parent().floatingScrollbar();
+  this.affectedElements.each(function() {
+    var element = $(this), scrollbarElement;
+
+    if (element.is('div.t-IRR-region')) {
+      // interactive report
+      scrollbarElement = element.find('table.a-IRR-table:last').parent();
+      apex.debug.info('Adding floating scrollbar to IR region.', scrollbarElement);
+      scrollbarElement.floatingScrollbar();
+    } else {
+      scrollbarElement = element.find('div.t-Region-body');
+      apex.debug.info('Adding floating scrollbar to region.', scrollbarElement);
+      scrollbarElement.floatingScrollbar();
+    }
+  });
 }
